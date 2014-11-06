e3a9a9d0d00dc52db01fbb70cdffecc934626ed9
Melvin <melvin0008@gmail.com>
2014-11-06 21:20:13 +0530

    changed host

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST = os.getenv("REDIS_HOST", "greeneye.redistogo.com")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "11156"))
 REDIS_DB = int(os.getenv("REDIS_DB", "1"))
