ff14bba322ff5c3b46360b0c94713030148da171
Melvin <melvin0008@gmail.com>
2014-11-06 21:47:00 +0530

    added urlparse

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+#!venv/bin/python
+from os import environ
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 redis_url = environ.get('REDISTOGO_URL')
 if redis_url: 
     url = urlparse(redis_url)
